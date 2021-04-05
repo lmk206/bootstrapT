c134f7bb6752bf46b3b01a797b0b4d3254a52dbb
lmk206 <leemingyo3@hanmail.net>
2021-04-05 13:30:11 +0900

    0405

---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.addEventListenner("DOMContentLoaded",function(){
+  //start
+
+  const header = document.querySelector('header');
+  const headerInner = document.querySelector('header .inner');  
+
+  //end
+})
